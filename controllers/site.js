b57e7a2698cfe793f1c737aa21ad8adea6115095
TTumyr <trond.tumyr@online.no>
2019-08-14 21:08:14 +0200

    Added user components login and register forms

--- controllers/site.js
+++ controllers/site.js
@@ -12,3 +12,25 @@ exports.home = (req, res, next) => {
       res.render('index.ejs', { items, system, pgTitle: 'nodejs', path: '/' });
     });
 };
+
+exports.login = (req, res, next) => {
+  if (req.method === 'GET') {
+    res.render('login.ejs', { system, pgTitle: 'login', path: '/login' });
+  } else if (req.method === 'POST') {
+    console.log('Add login logic here');
+    res.send('No login logic added yet.\n<a href="/login">back</a>');
+  }
+};
+
+exports.registerUser = (req, res, next) => {
+  if (req.method === 'GET') {
+    res.render('register_user.ejs', {
+      system,
+      pgTitle: 'register',
+      path: '/register-user'
+    });
+  } else if (req.method === 'POST') {
+    console.log('Add logic here');
+    res.send('No register possible yet.\n<a href="/register-user">back</a>');
+  }
+};
--- routes/routes.js
+++ routes/routes.js
@@ -5,5 +5,9 @@ const site = require('../controllers/site');
 const router = express.Router();
 
 router.get('/', site.home);
+router.get('/login', site.login);
+router.post('/login', site.login);
+router.get('/register-user', site.registerUser);
+router.post('/register-user', site.registerUser);
 
 module.exports = router;
