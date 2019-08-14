b5ce4748bf689c45426a37d854381bb623279877
TTumyr <trond.tumyr@online.no>
2019-08-15 00:49:34 +0200

    added user registration logic

--- controllers/site.js
+++ controllers/site.js
@@ -1,15 +1,17 @@
+const bcrypt = require('bcrypt');
 const mdb = require('../utils/mdb');
+//const User = require('../models/User');
 const settings = require('../utils/settings');
 const system = settings.system();
 
 exports.home = (req, res, next) => {
   mdb.query.operation = 'find';
-  mdb.query.collection = 'data1';
+  mdb.query.collection = 'users';
   mdb.db
     .collection(mdb.query.collection)
     .find()
-    .toArray((err, items) => {
-      res.render('index.ejs', { items, system, pgTitle: 'nodejs', path: '/' });
+    .toArray((err, users) => {
+      res.render('index.ejs', { users, system, pgTitle: 'nodejs', path: '/' });
     });
 };
 
@@ -31,6 +33,28 @@ exports.registerUser = (req, res, next) => {
     });
   } else if (req.method === 'POST') {
     console.log('Add logic here');
-    res.send('No register possible yet.\n<a href="/register-user">back</a>');
+    if (req.password === req.passwordR) {
+      mdb.query.operation = 'find';
+      mdb.query.collection = 'users';
+      let salt = bcrypt.genSaltSync(10);
+      let password = bcrypt.hashSync(req.body.password, salt);
+      console.log(password);
+      mdb.db
+        .collection(mdb.query.collection)
+        .insertOne({
+          username: req.body.username,
+          email: req.body.email,
+          password: password,
+          registered: new Date()
+        })
+        .then(data => {
+          console.log(data);
+        })
+        .then(() => {
+          res.redirect('/');
+        });
+    } else {
+      res.send('No register possible yet.\n<a href="/register-user">back</a>');
+    }
   }
 };
